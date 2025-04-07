21cca756c390b9cadcd1233bc8c511c3f4ef8641
jhyang22 <jaeho9229@gmail.com>
2025-04-07 20:04:50 +0900

    feat
    user entity 생성

--- src/main/java/com/example/sns_feed/user/entity/User.java
+++ src/main/java/com/example/sns_feed/user/entity/User.java
@@ -1,4 +1,42 @@
 package com.example.sns_feed.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String mobileNumber;
+
+    @Column(nullable = false)
+    private String brithDate;
+
+    private LocalDateTime deletedAt;
+
+    public User() {
+
+    }
+
+    public User(String email, String password, String mobileNumber, String brithDate, LocalDateTime deletedAt) {
+        this.email = email;
+        this.password = password;
+        this.mobileNumber = mobileNumber;
+        this.brithDate = brithDate;
+        this.deletedAt = deletedAt;
+    }
 }
