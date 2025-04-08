bb37efa73efdf7a87508f3d8d030b6fe82518465
AhyunCho <jah112482@gmail.com>
2025-04-08 21:24:55 +0900

    feat: entity 재수정 찐최종

--- src/main/java/com/example/sns_feed/board/SnsFeed.sql
+++ src/main/java/com/example/sns_feed/board/SnsFeed.sql
@@ -1,14 +1,13 @@
 use sns_feed;
 
-insert into user (create_at, deleted_at, id, updated_at, brith_date, email, mobile_number, password, profile, user_name)
+insert into users (create_at, deleted_at, id, updated_at, brith_date, email, mobile_number, password, profile, user_name)
 values (now(),null,1,now(),'19981124','test@naver.com','01000000000','test','image.jpg','테스트');
 
-select * from user;
+select * from users;
 
-select * from comment;
+select * from comments;
 
-select * from board;
-drop table users;
+select * from boards;
 
 DROP DATABASE sns_feed;
 CREATE DATABASE sns_feed CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
