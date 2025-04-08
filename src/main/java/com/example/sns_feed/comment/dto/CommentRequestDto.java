5c68c826de64307d5601b3b2ca39940c4cc7a131
hyejeongjin <hyejeongjin.dev@gmail.com>
2025-04-08 19:54:20 +0900

    feat:  comment 생성

--- src/main/java/com/example/sns_feed/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/sns_feed/comment/dto/CommentRequestDto.java
@@ -1,4 +1,13 @@
 package com.example.sns_feed.comment.dto;
 
+import lombok.Getter;
+
+@Getter
 public class CommentRequestDto {
+
+    private final String contents;
+
+    public CommentRequestDto(String contents) {
+        this.contents = contents;
+    }
 }
