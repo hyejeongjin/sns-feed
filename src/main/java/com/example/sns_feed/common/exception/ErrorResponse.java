01bb0e7c81a08745b0c69ff0cd331d6094779ec5
jhyang22 <jaeho9229@gmail.com>
2025-04-09 20:29:59 +0900

    feat : @JsonFormat 어노테이션 추가
    postman 출력 시 소수점 이하 삭제

--- src/main/java/com/example/sns_feed/common/exception/ErrorResponse.java
+++ src/main/java/com/example/sns_feed/common/exception/ErrorResponse.java
@@ -1,6 +1,7 @@
 package com.example.sns_feed.common.exception;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime timestamp = LocalDateTime.now();
     private final int status;
     private final String error;
