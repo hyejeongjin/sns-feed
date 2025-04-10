466e6f6ff46efce5d893de298db62548c2d78efb
jhyang22 <jaeho9229@gmail.com>
2025-04-11 03:07:20 +0900

    feat : 비밀번호 찾기 filter 2개 추가

--- src/main/java/com/example/sns_feed/config/WebConfig.java
+++ src/main/java/com/example/sns_feed/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.example.sns_feed.config;
 
+import com.example.sns_feed.config.filter.ChangePasswordFilter;
 import com.example.sns_feed.config.filter.LoginFilter;
+import com.example.sns_feed.config.filter.UserCERTFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,26 @@ public class WebConfig implements WebMvcConfigurer {
         FilterRegistrationBean<LoginFilter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginFilter()); // Filter 등록
         filterRegistrationBean.setOrder(1); // Filter 순서
+        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean UserCERTFilter() {
+        FilterRegistrationBean<UserCERTFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new UserCERTFilter()); // Filter 등록
+        filterRegistrationBean.setOrder(2); // Filter 순서
+        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean ChangePasswordFilter() {
+        FilterRegistrationBean<ChangePasswordFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new ChangePasswordFilter()); // Filter 등록
+        filterRegistrationBean.setOrder(3); // Filter 순서
         filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
 
         return filterRegistrationBean;
