276ecbaec4f972daeaa599bebad7f6d55ee15c2b
jhyang22 <jaeho9229@gmail.com>
2025-04-10 12:37:55 +0900

    fix : 어노테이션 수정

--- src/main/java/com/example/sns_feed/domain/user/dto/requestdto/UpdatePasswordRequestDto.java
+++ src/main/java/com/example/sns_feed/domain/user/dto/requestdto/UpdatePasswordRequestDto.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 @Getter
 public class UpdatePasswordRequestDto {
 
-    @NotBlank(message = "공백을 입력했습니다.")
-    @Size(min = 8, max = 20, message = "8~20자 입력해주세요.")
+    @Pattern(regexp ="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$!@*()-])[A-Za-z[0-9]$!@*()-]{8,20}",
+            message = "최소 8 ~ 20자 입력해주세요.")
     private final String oldPassword;
 
     @Pattern(regexp ="^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[$!@*()-])[A-Za-z[0-9]$!@*()-]{8,20}",
