f0d9c80b927597c91653fe6119cb8b91c14ed79d
jhyang22 <jaeho9229@gmail.com>
2025-04-11 21:08:12 +0900

    fix : 오탈자 수정

--- src/main/java/com/example/sns_feed/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/example/sns_feed/domain/comment/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package com.example.sns_feed.domain.comment.repository;
 
 import com.example.sns_feed.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
