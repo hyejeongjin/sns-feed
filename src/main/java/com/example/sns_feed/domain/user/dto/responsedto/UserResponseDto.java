dea21c9fc547770e0e58df0aaefafa502c6fd8eb
jhyang22 <jaeho9229@gmail.com>
2025-04-11 21:35:06 +0900

    fix : 오탈자 수정

--- src/main/java/com/example/sns_feed/domain/user/dto/responsedto/UserResponseDto.java
+++ src/main/java/com/example/sns_feed/domain/user/dto/responsedto/UserResponseDto.java
@@ -8,7 +8,6 @@ import java.io.Serializable;
 @Setter
 @Getter
 public class UserResponseDto implements Serializable {
-    private static final long serialVersionUID = 1L;
 
     private final Long id;
 
