e901eece3398c19910725e88b9c565fbb9c18a64
jhyang22 <jaeho9229@gmail.com>
2025-04-11 11:20:42 +0900

    fix : 안쓰는 파일 삭제

--- src/main/java/com/example/sns_feed/common/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.sns_feed.common.exception;
-
-public class UserNotFoundException extends CustomException {
-    public UserNotFoundException(String message) {
-        super(ErrorCode.USER_NOT_FOUND, message);
-    }
-}
