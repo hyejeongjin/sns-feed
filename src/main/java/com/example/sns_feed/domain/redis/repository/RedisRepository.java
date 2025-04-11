8e56d21b2e1b04b72770065f93f28a5927bb54b2
jhyang22 <jaeho9229@gmail.com>
2025-04-11 21:17:44 +0900

    fix : 사용안하는 파일 삭제(redis)

--- src/main/java/com/example/sns_feed/domain/redis/repository/RedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sns_feed.domain.redis.repository;
-
-import com.example.sns_feed.domain.redis.entitiy.RedisItem;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RedisRepository extends CrudRepository<RedisItem, String>  {
-
-}
