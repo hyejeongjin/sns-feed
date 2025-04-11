d9c8b9b3f6abebe11d5ffc5f14a3645fe15c6e5e
jhyang22 <jaeho9229@gmail.com>
2025-04-11 10:34:44 +0900

    fix : redis 관련 삭제

--- src/main/java/com/example/sns_feed/redis/UserRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.sns_feed.redis;
-
-import com.example.sns_feed.domain.user.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRedisRepository extends CrudRepository<User, Long> {
-
-}
