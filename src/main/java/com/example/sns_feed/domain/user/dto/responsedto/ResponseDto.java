dda64d6005f95d95546219d38751f8ce1de19931
jhyang22 <jaeho9229@gmail.com>
2025-04-11 13:57:02 +0900

    feat : 필드 추가

--- src/main/java/com/example/sns_feed/domain/user/dto/responsedto/ResponseDto.java
+++ src/main/java/com/example/sns_feed/domain/user/dto/responsedto/ResponseDto.java
@@ -9,13 +9,18 @@ import java.time.LocalDateTime;
 @Getter
 public class  ResponseDto {
 
+    private final String profile;
+
+    private final String userName;
+
     private final String email;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime updatedAt;
 
-
     public ResponseDto(User user) {
+        this.profile = user.getProfile();
+        this.userName = user.getUserName();
         this.email = user.getEmail();
         this.updatedAt = user.getUpdatedAt();
     }
