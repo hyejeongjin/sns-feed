5ab2a4b52dc3b3c3d6168b92888c375885248423
jhyang22 <jaeho9229@gmail.com>
2025-04-11 10:36:30 +0900

    fix : 필터 주석처리

--- src/main/java/com/example/sns_feed/config/WebConfig.java
+++ src/main/java/com/example/sns_feed/config/WebConfig.java
@@ -1,8 +1,6 @@
 package com.example.sns_feed.config;
 
-import com.example.sns_feed.config.filter.ChangePasswordFilter;
 import com.example.sns_feed.config.filter.LoginFilter;
-import com.example.sns_feed.config.filter.UserCERTFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,23 +18,23 @@ public class WebConfig implements WebMvcConfigurer {
         return filterRegistrationBean;
     }
 
-    @Bean
-    public FilterRegistrationBean UserCERTFilter() {
-        FilterRegistrationBean<UserCERTFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new UserCERTFilter()); // Filter 등록
-        filterRegistrationBean.setOrder(2); // Filter 순서
-        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
+//    @Bean
+//    public FilterRegistrationBean UserCERTFilter() {
+//        FilterRegistrationBean<UserCERTFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        filterRegistrationBean.setFilter(new UserCERTFilter()); // Filter 등록
+//        filterRegistrationBean.setOrder(2); // Filter 순서
+//        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
+//
+//        return filterRegistrationBean;
+//    }
 
-        return filterRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean ChangePasswordFilter() {
-        FilterRegistrationBean<ChangePasswordFilter> filterRegistrationBean = new FilterRegistrationBean<>();
-        filterRegistrationBean.setFilter(new ChangePasswordFilter()); // Filter 등록
-        filterRegistrationBean.setOrder(3); // Filter 순서
-        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
-
-        return filterRegistrationBean;
-    }
+//    @Bean
+//    public FilterRegistrationBean ChangePasswordFilter() {
+//        FilterRegistrationBean<ChangePasswordFilter> filterRegistrationBean = new FilterRegistrationBean<>();
+//        filterRegistrationBean.setFilter(new ChangePasswordFilter()); // Filter 등록
+//        filterRegistrationBean.setOrder(3); // Filter 순서
+//        filterRegistrationBean.addUrlPatterns("/*"); // 전체 URL에 Filter 적용
+//
+//        return filterRegistrationBean;
+//    }
 }
