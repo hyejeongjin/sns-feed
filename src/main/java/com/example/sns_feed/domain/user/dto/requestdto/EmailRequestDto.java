0da0b7f7ab3c13942d9eb9bf4d3907ec3fac2810
jhyang22 <jaeho9229@gmail.com>
2025-04-11 11:01:39 +0900

    feat : validation 추가

--- src/main/java/com/example/sns_feed/domain/user/dto/requestdto/EmailRequestDto.java
+++ src/main/java/com/example/sns_feed/domain/user/dto/requestdto/EmailRequestDto.java
@@ -1,10 +1,14 @@
 package com.example.sns_feed.domain.user.dto.requestdto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class EmailRequestDto {
 
+    @Email
+    @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "이메일 형식이 아닙니다")
     private final String email;
 
     public EmailRequestDto(String email) {
