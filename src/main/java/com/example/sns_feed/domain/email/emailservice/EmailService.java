2041679ad5cbbea4afd1c7635efe24c9f0c678ce
jhyang22 <jaeho9229@gmail.com>
2025-04-13 13:13:15 +0900

    feat : Transactional 어노테이션 추가

--- src/main/java/com/example/sns_feed/domain/email/emailservice/EmailService.java
+++ src/main/java/com/example/sns_feed/domain/email/emailservice/EmailService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -43,8 +44,8 @@ public class EmailService {
         return message;
     }
 
+    @Transactional
     public int sendMail(String mail){
-
         MimeMessage mimeMessage = CreateMail(mail);
         javaMailSender.send(mimeMessage);
         return number;
